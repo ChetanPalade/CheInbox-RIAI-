128cecd2695700f3443817235c45fdefd578cfba
ChetanPalade <147688599+ChetanPalade@users.noreply.github.com>
2024-08-26 11:53:30 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@
 
 function Header() {
   return (
-    <div className="bg-[#050101] fixed w-screen h-20 d-flex  items-center justify-center">
-      <div className="p-2 mt-2">
+    <div className="bg-[#050101] fixed w-screen h-16 flex items-center justify-center ">
+      <div className="p-2 mt-2 flex items-center justify-center text-white">
       <img src={"https://i.pinimg.com/564x/cf/c4/b6/cfc4b6c572448f1f5996b98b29a8ea44.jpg"} 
        alt="logo"
-       className="p-3 h-20 w-25"/>
+       className="p-2 h-12"/><span>CheBox</span>
+      
       </div>  
     </div>
   )
